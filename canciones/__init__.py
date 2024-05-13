cfd91c39048855d25a6a261e39c21342d176af0a
candianerea <SalmaCandia1@gmail.com>
2024-05-13 11:39:59 -0300

    avance con las listas

--- canciones/__init__.py
+++ canciones/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -10,3 +10,14 @@ with app.app_context():
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+@app.route('/cancion')
+def cancion():
+    base_datos = db.get_db()
+    consulta = """SELECT name FROM tracks"""
+
+    resultado = base_datos.execute(consulta)
+    lista_resultados = resultado.fetchall()
+    return render_template('cancion.html', cancion=lista_resultados)
+
--- canciones/db.py
+++ canciones/db.py
@@ -4,47 +4,53 @@ import sqlite3
 import click
 from flask import current_app, g
 
-db_folder = current_app.instance.path
+db_folder = current_app.instance_path
 db_name = 'spotify.sqlite'
 db_file = os.path.join(db_folder, db_name)
-db_sql_file = 'discos.sql'
+db_sql_file = 'datos.sql'
+
+def dict_factory(cursor, row):
+    """Arma un diccionario con los valores de la fila."""
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 def get_db():
-if 'db' not in g:
-g.db = sqlite3.connect(
-db_file,
-detect_types = sqlite3.PARSE_DECLTYPES
-)
-g.db.row_factory = sqlite3.Row
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+    db_file,
+    detect_types = sqlite3.PARSE_DECLTYPES
+    )
+    g.db.row_factory = dict_factory
 
-return g.db
+    return g.db
+
 
 
 def close_db(e=None):
-db = g.pop('db', None)
+    db = g.pop('db', None)
 
-if db is not None:
-db.close()
+    if db is not None:
+        db.close()
 
 def init_db():
 # ensure the instance folder exists
-try:
-os.makedirs(current_app.instance_path)
-except OSError:
-pass
+    try:
+        os.makedirs(current_app.instance_path)
+    except OSError:
+        pass
 
-db = get_db()
+    db = get_db()
 
-with current_app.open_resource(db_sql_file) as f:
-db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource(db_sql_file) as f:   
+        db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
 def init_db_command():
-"""Clear the existing data and create new tables."""
-init_db()
-click.echo('Initialized the database.')
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
 
 def init_app(app):
-app.teardown_appcontext(close_db)
-app.cli.add_command(init_db_command)
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
