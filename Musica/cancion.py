04bbac22e0101706ca4e04b62071d4c97616558c
nerea <candiasalma1@gmail.com>
2025-02-10 14:31:27 -0300

    avance con la ruta de canciones

--- Musica/cancion.py
+++ Musica/cancion.py
@@ -1,9 +1,24 @@
+from flask import Flask, render_template
+import sqlite3  
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
 @app.route('/cancion')
 def cancion():
-    base_datos = db.get_db()
+    base_datos = get_db()  
     consulta = """SELECT name FROM tracks"""
-
     resultado = base_datos.execute(consulta)
     lista_resultados = resultado.fetchall()
     return render_template('cancion.html', cancion=lista_resultados)
 
+def get_db():
+    conn = sqlite3.connect('ruta_a_tu_base_de_datos.db') 
+    conn.row_factory = sqlite3.Row  
+    return conn
+
+if __name__ == "__main__":
+    app.run(debug=True)
